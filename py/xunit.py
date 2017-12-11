d8241826e6a51819f2448df15b0b341b82c0ddac
Pommel Kurama <pommel.pg@gmail.com>
2017-12-11 21:38:20 +0900

    fix todo: make a record of log with WasRun.

--- py/xunit.py
+++ py/xunit.py
@@ -10,29 +10,17 @@ class TestCase:
         method = getattr(self, self.name)
         method()
 
-
 class WasRun(TestCase):
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
-        self.log = 'setUp'
+        self.log = 'setUp '
 
     def testMethod(self):
-        self.wasRun = 1
-
+        self.log = self.log + 'testMethod '
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun('testMethod')
+    def testTemplateMethod(self):
+        test = WasRun('testMethod')
+        test.run()
+        assert('setUp testMethod ' == test.log)
 
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
-
-
-TestCaseTest('testRunning').run()
-TestCaseTest('testSetUp').run()
+TestCaseTest('testTemplateMethod').run()
