71b42931037fc95f43c7f5eaa8a4091265f1c559
Pommel Kurama <pommel.pg@gmail.com>
2017-12-11 21:23:35 +0900

    apply pylint

--- py/xunit.py
+++ py/xunit.py
@@ -1,29 +1,38 @@
 class TestCase:
-  def __init__( self, name ):
-    self.name = name
-  def setUp( self ):
-    pass
-  def run( self ):
-    self.setUp()
-    method = getattr( self, self.name )
-    method()
+    def __init__(self, name):
+        self.name = name
 
-class WasRun( TestCase ):
-  def setUp( self ):
-    self.wasRun = None
-    self.wasSetUp = 1
-  def testMethod( self ):
-    self.wasRun = 1
+    def setUp(self):
+        pass
 
-class TestCaseTest( TestCase ):
-  def setUp( self ):
-    self.test = WasRun( 'testMethod' )
-  def testRunning( self ):
-    self.test.run()
-    assert( self.test.wasRun )
-  def testSetUp( self ):
-    self.test.run()
-    assert( self.test.wasSetUp )
+    def run(self):
+        self.setUp()
+        method = getattr(self, self.name)
+        method()
 
-TestCaseTest( 'testRunning' ).run()
-TestCaseTest( 'testSetUp' ).run()
+
+class WasRun(TestCase):
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
+        self.log = 'setUp'
+
+    def testMethod(self):
+        self.wasRun = 1
+
+
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun('testMethod')
+
+    def testRunning(self):
+        self.test.run()
+        assert(self.test.wasRun)
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+
+
+TestCaseTest('testRunning').run()
+TestCaseTest('testSetUp').run()
